25dc37c7c6f9026afec295a8b4a6a172d95b895d
Ley Roy <atiwensolomon@gmail.com>
2023-10-22 11:01:03 +0000

    fixd now

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,6 @@ import Typical from "react-typical";
 import { useForm } from "react-hook-form";
 
 // assets ussed
-import signup from "./asset/signup.png";
 import Ulogo from "./asset/Ulogo.png";
 import Usericon from "./asset/Usericon.png";
 import mail from "./asset/mail.png";
@@ -19,7 +18,6 @@ import padlock from "./asset/padlock.png";
 
 export default function Login() {
 	const [isMobleScreen, setIsMobileScreen] = useState(false);
-
 	const {
 		register,
 		handleSubmit,
@@ -31,36 +29,29 @@ export default function Login() {
 	};
 
 	return (
-		<div className="flex flex-col items-start justify-start w-screen mins-h-[100vh] md:flex-row m-0">
+		<div className=" flex flex-col bg-cover bg-center items-cneter justify-center w-screen h-full md:flex-row m-0">
 			<div
 				className={` ${
 					!isMobleScreen && "hidden"
-				} relative top-0 h-[100vh] w-screen flex-grow md:flex overflow-hidden`}
+				} relative h-[100vh] justify-between items-center container1 w-screen bg-im md:flex overflow-hidden`}
 			>
-				<div className=" absolute bottom-[5%]  top-6 sm:top-28 left-[10%] right-[10%] sm:gap-[24rem] flex items-center justify-between flex-col m-auto">
-					<img
-						src={Ulogo}
-						alt=""
-						className="top-0 w-3/5 md:w-1/4 h-1/4"
-					/>
-					<button
-						onClick={toggleSignUp}
-						className=" md:hidden rounded-md bg-[#FFFFFF] text-2xl font-bold py-2 px-4 w-60"
-					>
-						sign Up
-					</button>
+				<div className="flex flex-col my-auto items-center  justify-between m-auto h-[90vh]">
+					<div className=" mt-7 mx-auto ">
+						<img
+							src={Ulogo}
+							alt=""
+							className=" max-w-[27%] max-h-[27%] min-w-[130px] min-h-[130px] md:w-2/4 h-1/4"
+						/>
+					</div>
+						<button
+							onClick={toggleSignUp}
+							className=" md:hidden rounded-md bg-[#FFFFFF] text-2xl font-bold py-2 px-4 w-60"
+						>
+							sign Up
+						</button>
 				</div>
 
-				<div className="absolute mx-auto sm:left-[10%] sm:right-[0%] top-[30%] sm:top-[40%] md:top-[37%] p-5  flex items-center justify-center flex-col gap-10">
-					{/* <p className="text-3xl font-bold leading-10 text-white capitalize md:text-4xl">
-						Join Our Food Sales Community{" "}
-						<span className="text-teal-500">
-							Sign Up Now for Gourmet{" "}
-							<p className="font-extrabold text-rose-600">
-								Offers and More
-							</p>
-						</span>
-					</p> */}
+				<div className="absolute mx-auto top-[50%]  p-5  flex items-center justify-center">
 					<h1 className=" text-center text-[6vh] md:text-[4.5vw] text-[#FFFFFF] transition-transform ease-in-out duration-300">
 						<Typical
 							loop={Infinity}
@@ -75,26 +66,18 @@ export default function Login() {
 						/>
 					</h1>
 				</div>
-				<div />
-				<img
-					src={signup}
-					alt=""
-					className="w-full h-full overflow-hidden"
-				/>
-				<div />
 			</div>
-
 			<div
 				className={`w-full min-h-[100vh] flex  bg-[#340835] md:flex justify-center  items-center px-3  relative flex-grow `}
 			>
-				<div className="w-[94%] sm:min-w-[85%] md:min-w-[85%] sm:w-[90%] mx-auto h-[68%]  py-5 flex-col rounded-tl-[100px] flex justify-center items-center rounded-br-[100px]  bg-[#FFFFFF] ">
+				<div className="w-[94%]  sm:min-w-[85%] md:min-w-[85%] sm:w-[90%] mx-auto h-[70%]  py-5 flex-col rounded-tl-[100px] flex justify-center items-center rounded-br-[100px]  bg-[#FFFFFF] ">
 					{!isMobleScreen && (
 						<AiOutlineClose
 							onClick={toggleSignUp}
 							className="cursor-pointer -mr-[90%] md:hidden -mt-2 p-1 w-7 h-7 font-bold hover:scale-110 duration-200 text-xl text-red-700 rounded-full"
 						/>
 					)}
-					<div className="text-4xl font-bold text-[#340835] mb-6">
+					<div className="text-4xl font-bold text-[#340835] mb-6 ">
 						Sign Up
 					</div>
 					<form
@@ -234,12 +217,11 @@ export default function Login() {
 							type="submit"
 							className="bg-[#29002A] w-[67%] p-2 m-4 text-md rounded-[0.600rem] font-bold text-[#F0F0F0] hover:scale-95 duration-200 ease-in-out"
 						>
-							{" "}
 							Sign Up
 						</button>
 					</form>
-					<p className="md:text-[.77rem] text-[.58rem] m-1 font-light md:font-semibold text-[#29002A]">
-						Already Have an account?{" "}
+					<p className="md:text-[.77rem] text-[.58rem] mb-3 font-light md:font-semibold text-[#29002A]">
+						Already Have an account?
 						<span className="text-sm font-extrabold cursor-pointer">
 							LOGIN
 						</span>
